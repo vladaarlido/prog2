5464f1217fee8e360f71ed25388bec330f6a0421
vlada <vladislavamasina1@gmail.com>
2026-03-18 14:03:00 +0300

    prog2.c

--- prog2.c
+++ prog2.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 int main()
-{
-    printf("My name is Vlada");
-    getchar();
 
+{
+    int num = 5;    //объявляем переменную num = 5
+    int *pNum = &num;   //обявляяем переменную указатель pNum
+    printf("%p - %p\n", pNum, &num); //выводит содюуказателя pNum (&num), и адре &num
+    printf("%d\n, num");    //выводит адрес num
+    printf("%d\n", *pNum);  //выводит адрес num
+
+    getchar();     //чтобы терминал не вис вводим просто enter
     return 0;
 }
