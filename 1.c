6d1a295a2cc6d23529703cd7b409042d03218d4e
vlada <vladislavamasina1@gmail.com>
2026-03-19 19:20:44 +0300

    vlada/19

--- 1.c
+++ 1.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    printf("My name is %s!", "Vlada");
+char x[40];
+    printf("Hi, what is ur name?\n");
+    scanf("%s", &x);
+    printf("Welcome, %s!\n", x);
+
+int a;
+    printf("How old r u?\n");
+    scanf("%d", &a);
+    
+    if (a>=18) {printf("Ur great, welcome %s!\n", x);}
+    else if (a<18) {printf("Sorry %s, ur too young!\n", x);}
 
     getchar();
     return 0;
