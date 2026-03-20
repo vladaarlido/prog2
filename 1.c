e483e2e056ecec3bd92b598c369ff81cfb0e034c
vlada <vladislavamasina1@gmail.com>
2026-03-20 11:18:44 +0300

    добавила к диалогу функции игры \начало

--- 1.c
+++ 1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 int main()
+
 {
 char x[40]; //массив чтобы вписать имя 40 символов условно допустимое значение для имени
     printf("Hi, what is ur name?\n"); //программа здоровается ждёт ввод букв
@@ -14,8 +16,15 @@ int a;//делаем переменную a которая будет число
   
     else if (a<18 && a>=0) {printf("Sorry %s, ur too young!\n", x);}//если a меньше или равно 18 и а больше или равно 0 выводим вы слишком маленький
 
-    else if (a>=110) {printf("\"ERROR\"\n Try again\n");}//если а больше или равно 110 выводим тшибку
-    else if (a<=0) {printf("\"ERROR\"\n Try again\n");}//если а меньше или равно 0 выводим ошибку
+    else if (a>=110) {printf("\"ERROR\"\n Try again\n");return 0;}//если а больше или равно 110 выводим тшибку
+    else if (a<=0) {printf("\"ERROR\"\n Try again\n");return 0;}//если а меньше или равно 0 выводим ошибку
+
+char answer[3];
+    printf("Do u wanna play a math game %s? (yes\\no)\n", x);
+    scanf("%s", answer);
+    if (strcmp(answer, "yes")==0){printf("Ur welcome!\n");}
+    else if (strcmp(answer, "no")==0){printf("Fine, bye %s!\n", x);return 0;}
+    else {printf("ERROR\n");return 0;}
 
     getchar();//ждёт ввода enter, просто чтобы не зависал терминал
     return 0;
